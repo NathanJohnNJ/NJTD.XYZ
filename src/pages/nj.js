277b1ca059badfa4ae17559ed06581f74e71c865
Nathan John <codesnj@gmail.com>
2023-09-24 10:49:33 +0100

    Edited and added colourschemes. Started to populate tje HMUA page

--- src/pages/nj.js
+++ src/pages/nj.js
@@ -1,50 +1,49 @@
 import { motion } from 'framer-motion';
-import './style.css'
-import { Typewriter } from 'react-simple-typewriter'
+import './style.css';
+import Typewriter from 'typewriter-effect';
+
+
 
 const Nj = () => {
 
-        const typeWriter = () => {
-                return(
-            <div className="aboutTop">
-                <div className="aboutTopLeft">
-                    <Typewriter 
-                    words={["Hi! I'm "]}
-                    typeSpeed="50"
-                    delaySpeed="1000"
-                    /> 
-                    <span className="boxContent">
-                        <Typewriter
-                        words={["Nathan."]} 
-                        typeSpeed="50"
-                        delaySpeed="1200"
-                        />
-                    </span>
-                    </div>
-                        <div className="aboutTopMiddle">
-                            <Typewriter 
-                            words={[" And I hate to be put in a box..."]}
-                            typeSpeed="50"
-                            delaySpeed="1300"
-                            />
-                        </div>
-                        <div className="aboutTopRight">
-                            <p>
-                                <Typewriter 
-                                words={[" ...but try as I might if you're reading this, you're probably going to be categorising me in some way. So if you insist on putting me in a box, can you at least make it pink, and fun, and unexpectedly delightful? Thanks!"]}
-                                typeSpeed="1000"
-                                delaySpeed="1800"
-                                />
-                            </p>
-                        </div>
-                    </div>)
-        }
+    function first(){
+        <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString("Hi I'm ")
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2500)
+      .start();
+        }}
+        />
+    }
+    function second(){
+        <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString("And I hate to be put in a box... ")
+      
+      .pauseFor(2500)
+      .start();
+        }}
+        />
+    }
+    function third(){
+        <Typewriter
+  onInit={(typewriter) => {
+    typewriter.typeString(" ...but try as I might if you're reading this, you're probably going to be categorising me in some way. So if you insist on putting me in a box, can you at least make it pink, and fun, and unexpectedly delightful? Thanks! ")
+      
+      .pauseFor(2500)
+      .start();
+        }}
+        />
+    }
 
 
     return (
         <div className="njcodes">
             <motion.div
-            className="container text-center  bg-black"
+            className="container text-center bg-black"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
@@ -52,14 +51,28 @@ const Nj = () => {
             >
                 <h1 className="njTitle">ABOUT NJ</h1>
                 <div className="aboutMe">
-                {typeWriter()}
+                <div className="aboutTop">
+                <div className="aboutTopLeft">
+                   Hi! I'm 
+                    <span className="boxContent">
+                        Nathan.
+                    </span>
+                </div>
+                <div className="aboutTopMiddle">
+                        And I hate to be put in a box...
+                </div>
+                <div className="aboutTopRight">
+                    <p>
+                            ...but try as I might if you're reading this, you're probably going to be categorising me in some way. So if you insist on putting me in a box, can you at least make it pink, and fun, and unexpectedly delightful? Thanks!
+                    </p>
+                </div>
+            </div>
                 
                 <p>Working in both creative and corporate industries has provided me with a full range of experience and tools that has led me to this path. I enjoy learning and am proud to say that I still push myself to learn every day. I have experience working with CSS, HTML, JavaScript, ReactJS, MicroPython, Python, and Yaml. I am also experienced on Apple, Linux, Rasperry Pi and Windows systems with a focus on Linux-based distriutions. I am passionate about the work I do and enjoy discussing opportunities for the next exciting challenge in my career.</p>
                 <p>My intention is for you to enjoy my portfolio, and this web app, as much as I have enjoyed putting it together. However, I'm afraid despite my creative past, I'm a lot more clued up with the back-end side of things, so forgive me if some of the images/colour schemes aren't as bob on as they could be. But I'm always open to a suggestion or a nod in the right direction!</p>
                 </div>
             </motion.div>
         </div>
-    )
-};
-
+    );
+    }
 export default Nj;
