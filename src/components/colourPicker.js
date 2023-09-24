95ca7245841f62d4531d7d8eba15eebf45f712bf
Nathan John <codesnj@gmail.com>
2023-09-24 03:38:08 +0100

    Changed colour schemes

--- src/components/colourPicker.js
+++ src/components/colourPicker.js
@@ -6,33 +6,37 @@ const ColourPicker = () => {
         r.style.setProperty(col, hex);
     }
 
-    function defaultClickHandle() {
-        setColour("--mainBG",'#D6D7DC')
-        setColour("--secondaryBG", '#9B9CA1')
-        setColour("--labelCol", '#EAB42C')
-        setColour("--accentCol", '#CA9206')
-        setColour("--titleCol", '#011039')
-        setColour("--infoCol", '#0A0A0C')
+    function blueClickHandle() {
+        setColour("--mainBG",'#F7FFFF')
+        setColour("--secondaryBG", '#042AC1')
+        setColour("--infoCol", '#C8C9DD')
+        setColour("--accentCol", '#042AC1')
+        setColour("--titleCol", '#092271')
     }
     function redClickHandle() {
-        setColour("--mainBG",'#FCEAE4')
-        setColour("--secondaryBG", '#F6CBC8')
-        setColour("--labelCol", '#CF8680')
-        setColour("--accentCol", '#F4846E')
-        setColour("--titleCol", '#C5BFB2')
-        setColour("--infoCol", '#979588')
+        setColour("--mainBG",'#FFF0EF')
+        setColour("--secondaryBG", '#FFDDD5')
+        setColour("--infoCol", '#C3806E')
+        setColour("--accentCol", '#AB260C')
+        setColour("--titleCol", '#8A130C')
+    }
+    function greenClickHandle() {
+        setColour("--mainBG",'#F3FEEA')
+        setColour("--secondaryBG", '#CAE1C7')
+        setColour("--infoCol", '#8EB68A')
+        setColour("--accentCol", '#2E8831')
+        setColour("--titleCol", '#19671D')
     }
     function greyScaleClickHandle() {
-        setColour("--mainBG",'#00000')
-        setColour("--secondaryBG", '#313131')
-        setColour("--labelCol", '#616161')
-        setColour("--accentCol", '#F2F2F2')
-        setColour("--titleCol", '#929292')
-        setColour("--infoCol", '#FFFFFF')
+        setColour("--mainBG",'#F2F2F2')
+        setColour("--secondaryBG", '#929292')
+        setColour("--infoCol", '#616161')
+        setColour("--accentCol", '#313131')
+        setColour("--titleCol", '#000000')
     }
     return(
         <div className="colourPicker">
-            <div className="set1" onClick={defaultClickHandle}>
+            <div className="set1" onClick={blueClickHandle}>
                 <div className="col1"></div>
                 <div className="col2"></div>
                 <div className="col3"></div>
@@ -40,7 +44,7 @@ const ColourPicker = () => {
                 <div className="col5"></div>
                 <div className="col6"></div>
             </div>
-            <div className="set2" onClick={redClickHandle}>
+            <div className="set2" onClick={greenClickHandle}>
                 <div className="col1"></div>
                 <div className="col2"></div>
                 <div className="col3"></div>
@@ -48,7 +52,15 @@ const ColourPicker = () => {
                 <div className="col5"></div>
                 <div className="col6"></div>
             </div>
-            <div className="set3" onClick={greyScaleClickHandle}>
+            <div className="set3" onClick={redClickHandle}>
+                <div className="col1"></div>
+                <div className="col2"></div>
+                <div className="col3"></div>
+                <div className="col4"></div>
+                <div className="col5"></div>
+                <div className="col6"></div>
+            </div>
+            <div className="set4" onClick={greyScaleClickHandle}>
                 <div className="col1"></div>
                 <div className="col2"></div>
                 <div className="col3"></div>
