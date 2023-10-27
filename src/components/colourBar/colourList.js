b6b0f8faf3cf95376e2d215d010586d2f79d43f9
Nathan John <codesnj@gmail.com>
2023-10-27 20:22:12 +0100

    Updated colour picker and media queries to improve adaptability

--- src/components/colourBar/colourList.js
+++ src/components/colourBar/colourList.js
@@ -1,10 +1,27 @@
 import '../style.css';
+import Modal from 'react-modal';
 import { useState } from 'react';
 
-const ColourPicker = () => {
+const ColourPickerList = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     const [colours, setColours] = useState(['#F7FFFF', '#E2EBF1', '#C8C9DD', '#042AC1', '#092271']);
     const [darkMode, setDarkMode] = useState(false);
 
+    function openModal(){
+        setModalIsOpen(true)
+    }
+    function closeModal(){
+        setModalIsOpen(false)
+    }
+    function mouseOver(){
+        const img = document.getElementById('listBtn')
+        img.style.transform = "rotate(270deg)"
+    }
+    function mouseOut(){
+        const img = document.getElementById('listBtn')
+        img.style.transform = "rotate(0deg)"
+    }
+    
     function setColour(col, hex){
         const r = document.querySelector(':root');
         r.style.setProperty(col, hex);
@@ -26,6 +43,7 @@ const ColourPicker = () => {
             setColour("--titleCol", '#F7FFFF')
             setColour("--borderCol", "#FFFFFF")
         }
+        setModalIsOpen(false)
     }
     function redClickHandle() {
         setColours(['#FFF0EF', '#FFDDD5', '#C3806E', '#AB260C', '#8A130C']);
@@ -44,6 +62,7 @@ const ColourPicker = () => {
             setColour("--titleCol", '#FFF0EF')
             setColour("--borderCol", "#FFFFFF")
         }
+        setModalIsOpen(false)
     }
     function greenClickHandle() {
         setColours(['#F3FEEA', '#CAE1C7', '#8EB68A', '#2E8831', '#19671D']);
@@ -62,6 +81,7 @@ const ColourPicker = () => {
             setColour("--titleCol", '#F3FEEA')
             setColour("--borderCol", "#FFFFFF")
         }
+        setModalIsOpen(false)
     }
     function orangeClickHandle() {
         setColours(['#FFEFD1', '#EFD3AC', '#D5A677', '#DB7B1D', '#DB5816']);
@@ -80,29 +100,28 @@ const ColourPicker = () => {
             setColour("--titleCol", '#FFEFD1')
             setColour("--borderCol", "#FFFFFF")
         }
+        setModalIsOpen(false)
     }
     function greyScaleClickHandle() {
         setColours(['#F2F2F2', '#929292', '#616161', '#313131', '#000000']);
-        darkLightClickHandle(['#F2F2F2', '#929292', '#616161', '#313131', '#000000'])
-        // if(darkMode===false){
-        //     setColour("--mainBG",'#F2F2F2')
-        //     setColour("--secondaryBG", '#929292')
-        //     setColour("--infoCol", '#616161')
-        //     setColour("--accentCol", '#313131')
-        //     setColour("--titleCol", '#000000')
-        //     setColour("--borderCol", "#000000")
-        // }else{
-        //     setColour("--mainBG",'#000000')
-        //     setColour("--secondaryBG", '#313131')
-        //     setColour("--infoCol", '#616161')
-        //     setColour("--accentCol", '#929292')
-        //     setColour("--titleCol", '#F2F2F2')
-        //     setColour("--borderCol", "#FFFFFF")
-        // }
+        if(darkMode===false){
+            setColour("--mainBG",'#F2F2F2')
+            setColour("--secondaryBG", '#929292')
+            setColour("--infoCol", '#616161')
+            setColour("--accentCol", '#313131')
+            setColour("--titleCol", '#000000')
+            setColour("--borderCol", "#000000")
+        }else{
+            setColour("--mainBG",'#000000')
+            setColour("--secondaryBG", '#313131')
+            setColour("--infoCol", '#616161')
+            setColour("--accentCol", '#929292')
+            setColour("--titleCol", '#F2F2F2')
+            setColour("--borderCol", "#FFFFFF")
+        }
+        setModalIsOpen(false)
     }
-    function darkLightClickHandle(arr) {
-        setColours([arr[0], arr[1], arr[2], arr[3], arr[4]]);
-        console.log(colours);
+    function darkLightClickHandle() {
         if(darkMode===false){
             setColour("--mainBG",`${colours[4]}`)
             setColour("--secondaryBG", `${colours[3]}`)
@@ -120,51 +139,62 @@ const ColourPicker = () => {
             setColour("--borderCol", "#000000")
             setDarkMode(false)
         }
+        setModalIsOpen(false)
     }
-
+    
     return(
-        <div className="colourPicker" id="colourPicker">
-            <div className="blueSet" onClick={blueClickHandle}>
-                <div className="col1"></div>
-                <div className="col2"></div>
-                <div className="col3"></div>
-                <div className="col4"></div>
-                <div className="col5"></div>
-            </div>
-            <div className="greenSet" onClick={greenClickHandle}>
-                <div className="col1"></div>
-                <div className="col2"></div>
-                <div className="col3"></div>
-                <div className="col4"></div>
-                <div className="col5"></div>
-            </div>
-            <div className="orangeSet" onClick={orangeClickHandle}>
-                <div className="col1"></div>
-                <div className="col2"></div>
-                <div className="col3"></div>
-                <div className="col4"></div>
-                <div className="col5"></div>
-            </div>
-            <div className="redSet" onClick={redClickHandle}>
-                <div className="col1"></div>
-                <div className="col2"></div>
-                <div className="col3"></div>
-                <div className="col4"></div>
-                <div className="col5"></div>
-            </div>
-            <div className="greySet" onClick={greyScaleClickHandle}>
-                <div className="col1"></div>
-                <div className="col2"></div>
-                <div className="col3"></div>
-                <div className="col4"></div>
-                <div className="col5"></div>
-            </div>
-            <div className="darkLight" onClick={darkLightClickHandle}>
-                <div className="dark"></div>
-                <div className="light"></div>
-            </div>
+        <div className="cPList">
+            <button id="listBtn" className="pickerBtn" onClick={openModal} onMouseOver={mouseOver} onMouseOut={mouseOut}></button>
+            <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            ariaHideApp={false}
+            className="listModal"
+            >
+                <div className="pickerList">
+                    <div className="blueSet" onClick={blueClickHandle}>
+                        <div className="col1"></div>
+                        <div className="col2"></div>
+                        <div className="col3"></div>
+                        <div className="col4"></div>
+                        <div className="col5"></div>
+                    </div>
+                    <div className="greenSet" onClick={greenClickHandle}>
+                        <div className="col1"></div>
+                        <div className="col2"></div>
+                        <div className="col3"></div>
+                        <div className="col4"></div>
+                        <div className="col5"></div>
+                    </div>
+                    <div className="orangeSet" onClick={orangeClickHandle}>
+                        <div className="col1"></div>
+                        <div className="col2"></div>
+                        <div className="col3"></div>
+                        <div className="col4"></div>
+                        <div className="col5"></div>
+                    </div>
+                    <div className="redSet" onClick={redClickHandle}>
+                        <div className="col1"></div>
+                        <div className="col2"></div>
+                        <div className="col3"></div>
+                        <div className="col4"></div>
+                        <div className="col5"></div>
+                    </div>
+                    <div className="greySet" onClick={greyScaleClickHandle}>
+                        <div className="col1"></div>
+                        <div className="col2"></div>
+                        <div className="col3"></div>
+                        <div className="col4"></div>
+                        <div className="col5"></div>
+                    </div>
+                    <div className="darkLight" onClick={darkLightClickHandle}>
+                        <div className="dark"></div>
+                        <div className="light"></div>
+                    </div>
+                </div>
+            </Modal>
         </div>
     )
-}
-
-export default ColourPicker;
+};
+        
+export default ColourPickerList;
