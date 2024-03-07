63d84b64cf8d52cfca3d5362fc25bf982dea6086
Nathan John <codesnj@gmail.com>
2024-03-07 14:03:17 +0000

    Updated portfolio

--- src/components/animated/animatedPortfolio/reactjs.js
+++ src/components/animated/animatedPortfolio/reactjs.js
@@ -1,7 +1,9 @@
 import calculator from './images/calc.png';
 import cats from './images/cats.png';
 import cookies from './images/cookies.png';
-import Cards from './portRight'
+import dice from './images/dice2.png';
+import NJTD from './images/njtd.png';
+import Cards from './portRight';
 
 const ReactJS = () => {
 
@@ -9,14 +11,14 @@ const ReactJS = () => {
         title: "Cats 4 Lyf",
         image: cats,
         link: "https://cats.njtd.xyz",
-        skills: ["HTML", "JSX", "CSS", "JavaScript", "ReactJS"],
+        skills: ["HTML", "JSX", "CSS", "JavaScript", "ReactJS", "React-Modal",],
         git: "https://github.com/NathanJohnNJ/cats"
       }
       const cookiesSite = {
         title: "The Cookie Club",
         image: cookies,
         link: "https://cookies.njtd.xyz",
-        skills: ["ReactJS", "JavaScript", "JSX", "CSS", "HTML", "API", "Express", "NodeJS", "Cors", "dotEnv"],
+        skills: ["ReactJS", "React-Modal", "JavaScript", "JSX", "CSS", "HTML", "API", "Express", "NodeJS", "Cors", "dotEnv"],
         git: "https://github.com/NathanJohnNJ/cookies"
       }
       const calcSite = {
@@ -26,8 +28,22 @@ const ReactJS = () => {
         skills: ["ReactJS", "JavaScript", "JSX", "CSS", "HTML"],
         git: "https://github.com/NathanJohnNJ/calc"
       }
+      const diceSite = {
+        title: "Green Bottles Dice Game - Remix",
+        image: dice,
+        link: "https://diceremix.njtd.xyz",
+        skills: ["ReactJS", "React-Modal", "JavaScript", "JSX", "CSS", "HTML"],
+        git: "https://github.com/NathanJohnNJ/diceGameRemix"
+      }
+      const njtdSite = {
+        title: "NJTD.XYZ",
+        image: NJTD,
+        link: "https://www.njtd.xyz",
+        skills: ["ReactJS", "React-Modal", "Framer Motion", "Styled Components", "JavaScript", "JSX", "CSS", "HTML"],
+        git: "https://github.com/NathanJohnNJ/njtd.xyz"
+      }
   
-    const cardList = [calcSite, catsSite, cookiesSite]
+    const cardList = [calcSite, catsSite, cookiesSite, diceSite, njtdSite]
      
 
     return (
