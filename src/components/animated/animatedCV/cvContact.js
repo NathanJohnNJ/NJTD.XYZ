dda1f5c596fe7f88be70c082a2a2d7e231def043
Nathan John <codesnj@gmail.com>
2024-03-13 18:41:29 +0000

    Added map to CV.

--- src/components/animated/animatedCV/cvContact.js
+++ src/components/animated/animatedCV/cvContact.js
@@ -5,15 +5,25 @@ import git from './images/github.png';
 import linkedin from './images/linkedin.png';
 import home from './images/address.png';
 import web from './images/website.png';
+import Modal from 'react-modal';
+import { useState } from 'react';
 
 const CVContact = () => {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    function openModal(){
+        setModalIsOpen(true)
+    }
+    function closeModal(){
+        setModalIsOpen(false)
+    }
 
     return(
         <div className="cvContact">
             <div className="cvContactTitle">
                 <span className="cvContactListTitle">Contact</span>
             </div>
-            <div className="address">
+            <div className="address" onClick={openModal}>
                 <img src={home} alt="Address" className="contactImg"></img>
                 <span className="contactListItem">Great Sankey, Warrington, WA5</span>
             </div>
@@ -37,6 +47,17 @@ const CVContact = () => {
                 <img src={linkedin} alt="LinkedIn" className="contactImg"></img>
                 <a className="contactListItem" href="https://www.linkedin.com/in/nathanjohnnj" target="_blank" rel="noreferrer">www.linkedin.com/in/nathanjohnnj</a>
             </div>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                ariaHideApp={false}
+                className="mapModal"
+                >
+                <div className="mapModalDiv">
+                    <div className="close" onClick={closeModal}></div>
+                    <iframe title="My Location" width="600" height="800" className="map" loading="lazy" allowfullscreen src="https://storage.googleapis.com/maps-solutions-czgorco3yq/locator-plus/yfyn/locator-plus.html"></iframe>
+                </div>
+            </Modal>
         </div>
     )
 };
--- src/components/animated/animatedPortfolio/portLeft.js
+++ src/components/animated/animatedPortfolio/portLeft.js
@@ -22,66 +22,64 @@ return (
   transition={{ duration: 0.5, ease: "easeIn"}}
   >
     <div className="divWithTitle">
-    <div className="titleContainer">
-      {(props.title)?addTitle(props.title):console.log("no title")}
-    </div>
-    <div className="newMainDiv" style={{ position: 'relative', display: 'flex', width: "fitContent"}}>
-      {props.cards.map((card, i) => {
-      return(
-        <motion.div key={i} className="newIndividualDiv"
-          variants={{
-            hidden: { y: 200, opacity: 0 },
-            visible: { y: 0, opacity: 1 },
-          }}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{  margin: "-150px" }}
-          transition={{ duration: 1.5, ease: "easeIn", type: "spring", bounce: 0.4}}
-          >
-  <motion.div
-      className="card-container"
-      key={i}
-      viewport={{ once: true, amount: 0.8 }}
-    >
-      <div className="splash">
-        <p className="fadedTitle">{props.title}</p>
+      <div className="titleContainer">
+        {(props.title)?addTitle(props.title):console.log("no title")}
       </div>
-      <motion.div className="card"
-      variants={{
-        hidden: { y: 300, opacity: 0 },
-        visible: {
-            y: 50,
-            rotate: -10,
-            opacity: 1,
-            transition: {
-              type: "spring",
-              bounce: 0.2,
-              duration: 0.8,
-            }
-          },
-      }}
-      initial="hidden"
-      whileInView="visible"
-      >
-        <h1>{card.title}</h1>
-        <img src={card.image} alt={card.title}></img>
-        <h2><a href={card.link}>Click here to view the app yourself!</a></h2>
-        <h2><a href={card.git}>Or view the code on GitHub</a></h2>
-        
-        <h3>Skills/Languages used:</h3>
-          <ul>
-          {card.skills.map((skill, i) => {
-            return(
-            <li key={i}>{skill}</li>
-            )
-          })}
-          </ul>
-      </motion.div>
-    </motion.div>
-      
-            </motion.div>)
-      })}
-    </div>
+      <div className="newMainDiv" style={{ position: 'relative', display: 'flex', width: "fitContent"}}>
+        {props.cards.map((card, i) => {
+        return(
+          <motion.div key={i} className="newIndividualDiv"
+            variants={{
+              hidden: { y: 200, opacity: 0 },
+              visible: { y: 0, opacity: 1 },
+            }}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{  margin: "-150px" }}
+            transition={{ duration: 1.5, ease: "easeIn", type: "spring", bounce: 0.4}}
+            >
+          <motion.div
+            className="card-container"
+            key={i}
+            viewport={{ once: true, amount: 0.8 }}
+          >
+            <div className="splash">
+              <p className="fadedTitle">{props.title}</p>
+            </div>
+            <motion.div className="card"
+            variants={{
+              hidden: { y: 300, opacity: 0 },
+              visible: {
+                  y: 50,
+                  rotate: -10,
+                  opacity: 1,
+                  transition: {
+                    type: "spring",
+                    bounce: 0.2,
+                    duration: 0.8,
+                  }
+                },
+            }}
+            initial="hidden"
+            whileInView="visible"
+            >
+              <h1>{card.title}</h1>
+              <img src={card.image} alt={card.title}></img>
+              <h2><a href={card.link}>Click here to view the app yourself!</a></h2>
+              <h2><a href={card.git}>Or view the code on GitHub</a></h2> 
+              <h3>Skills/Languages used:</h3>
+              <ul>
+                {card.skills.map((skill, i) => {
+                  return(
+                    <li key={i}>{skill}</li>
+                  )
+                })}
+              </ul>
+            </motion.div>
+          </motion.div>
+        </motion.div>)
+        })}
+      </div>
     </div>
   </motion.div>
 );
